9add086a7778fcf02e4c9fcf0df7fe829b30b2ba
Millo <48436562+vukieuhaihoa@users.noreply.github.com>
2026-04-15 13:33:33 +0700

    feat: database setup, configuration and sql migrations (#2)

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/vukieuhaihoa/greenlight
 go 1.26.1
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.12.3 // indirect
---
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE movies DROP CONSTRAINT movies_runtime_check;
+
+ALTER TABLE movies DROP CONSTRAINT movies_year_check;
+
+ALTER TABLE movies DROP CONSTRAINT movies_genres_length_check;

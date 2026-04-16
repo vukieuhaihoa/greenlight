e0f1b2d76d14537c7738f229d4308e7f005e354b
Vu Kieu Hai Hoa <vukieuhaihoa@gmail.com>
2026-04-16 16:48:56 +0700

    feat: add graceful shutdown

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -11,6 +11,8 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version": version,
 	}
 
+	// time.Sleep(4 * time.Second)
+
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
---
+++ cmd/api/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  time.Minute,
+		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+	}
+
+	shutdownErr := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-quit
+
+		app.logger.Info("shutting down server", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownErr <- srv.Shutdown(ctx)
+	}()
+
+	app.logger.Info("starting server", "version", version, "port", app.config.port, "env", app.config.env)
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownErr
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("stopped server", "port", app.config.port)
+
+	return nil
+}

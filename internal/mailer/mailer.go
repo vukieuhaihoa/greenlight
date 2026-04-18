4bcc21d5ad7165340dacd4706de32f7b3765b8c0
Millo <48436562+vukieuhaihoa@users.noreply.github.com>
2026-04-18 16:10:11 +0700

    feat: add user register and sending email (#8)

---
+++ internal/mailer/mailer.go
@@ -0,0 +1,74 @@
+package mailer
+
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"time"
+
+	"github.com/go-mail/mail/v2"
+)
+
+//go:embed "templates"
+var templateFS embed.FS
+
+type Mailer struct {
+	dialer *mail.Dialer
+	sender string
+}
+
+func New(host string, port int, username, password, sender string) Mailer {
+	dialer := mail.NewDialer(host, port, username, password)
+	dialer.Timeout = 5 * time.Second
+
+	return Mailer{
+		dialer: dialer,
+		sender: sender,
+	}
+}
+
+func (m Mailer) Send(recipient, templateFile string, data any) error {
+	tmpl, err := template.New("email").ParseFS(templateFS, "templates/"+templateFile)
+	if err != nil {
+		return err
+	}
+
+	subject := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(subject, "subject", data)
+	if err != nil {
+		return err
+	}
+
+	plainBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(plainBody, "plainBody", data)
+	if err != nil {
+		return err
+	}
+
+	htmlBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(htmlBody, "htmlBody", data)
+	if err != nil {
+		return err
+	}
+
+	msg := mail.NewMessage()
+	msg.SetHeader("From", m.sender)
+	msg.SetHeader("To", recipient)
+	msg.SetHeader("Subject", subject.String())
+	msg.SetBody("text/plain", plainBody.String())
+	msg.AddAlternative("text/html", htmlBody.String())
+
+	// Try to send the email up to 5 times, with a 500ms pause between attempts. If
+	// the email is sent successfully then we break out of the loop and return a nil error. If we exhaust all 5 attempts and the email still hasn't been sent, then we return
+	// the error from the final attempt.
+	for i := 1; i <= 5; i++ {
+		err = m.dialer.DialAndSend(msg)
+		if err == nil {
+			break
+		}
+
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	return err
+}
